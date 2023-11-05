5c438297648c14889485ba85ced2d0066d3911e5
jaijoshi2712 <jai.j2712@gmail.com>
2023-11-04 20:11:32 -0400

    feat: Added JS and CSS component

---
+++ v1/scripts/content.js
@@ -0,0 +1,12 @@
+let body = document.querySelector("body");
+let btn = document.createElement("button");
+btn.setAttribute("id", "btn");
+btn.addEventListener("click", doATask);
+body.appendChild(btn);
+
+
+function doATask(){
+
+    
+}
+
