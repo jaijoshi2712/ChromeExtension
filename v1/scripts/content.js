81f6d885145a407be312cd21891becf5b1fbdba6
jaijoshi2712 <jai.j2712@gmail.com>
2023-11-04 22:15:43 -0400

    scripts refined, Speech recognition API added

--- v1/scripts/content.js
+++ v1/scripts/content.js
@@ -4,9 +4,38 @@ btn.setAttribute("id", "btn");
 btn.addEventListener("click", doATask);
 body.appendChild(btn);
 
+let speechRecognition = new webkitSpeechRecognition()
+speechRecognition.continuous = true;
+speechRecognition.interimResults = true;
+speechRecognition.lang = "en-us";
+
+let transcript = "";
+speechRecognition.onresult = function(event) {
+    transcript = "";
+    for (let i =0;i<event.results.length; ++i){
+        transcript+=event.results[i][0].transcript;
+    }
+};
+
+
 
 function doATask(){
 
-    
+    if (btn.hasAttribute("listening")=== false){
+        btn.setAttribute("listening", true);
+        speechRecognition.start();
+    }
+    else{
+        btn.removeAttribute("listening");
+        speechRecognition.stop();
+        const myPopup = new Popup({
+            id: "my-popup",
+            title: "Here is what you said:",
+            content: transcript
+        });
+        myPopup.show();
+    }
+
+
 }
 
